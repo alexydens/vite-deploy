9a5908f329e733e93c9ce6269862b08d92411b24
alexydens <alexydens@outlook.com>
2023-08-06 16:50:41 +0100

    A simple button example in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  );
 };
 
 export default App;
